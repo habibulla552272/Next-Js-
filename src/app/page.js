3e24ca9c4fea709ce41ea0e485bf767d8b2b9887
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-03-16 23:56:35 +0600

    some change

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,26 @@
 import Image from "next/image";
 import './globals.css';
+import { Poppins } from "next/font/google";
+import nextsvg from "/public/IMG-1169-removebg-preview.png"
+
+
+ const Poppin =Poppins({
+  subsets:['latin'],
+  weight: ['200','700']
+ })
+
 
 export default function Home() {
   return (
      <div>
 
-        <h2 className="h2">
+        <h2 className={Poppin}>
           Hey bro how are this is a next js project
         </h2>
+        <h2 className={`${Poppin.className} font-bold`}>
+          hey bro how are this is next js project
+        </h2>
+        <Image src={nextsvg}/>
      </div>
     
   );
